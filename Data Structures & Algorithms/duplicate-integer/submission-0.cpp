0c12eba1a5c3c6cddd8fb9a04d68adee76adc959
Ritika-Maan <ritikamaan2006@gmail.com>
2026-08-04 11:33:55 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++){
+            if(mp.count(nums[i])){
+                return true;
+                break;
+            }
+            else{
+                mp[nums[i]]+=1;
+            }
+        }
+        return false;
+    }
+};
